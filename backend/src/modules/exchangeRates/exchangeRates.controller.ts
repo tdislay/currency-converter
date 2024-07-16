6c7c1995425f0929511d2da672b152306f220ca8
Théo Dislay <mathdis@hotmail.fr>
2024-07-16 17:21:43 +0200

    fix: no cache for GET routes

--- backend/src/modules/exchangeRates/exchangeRates.controller.ts
+++ backend/src/modules/exchangeRates/exchangeRates.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Res } from "@nestjs/common";
+import { Response } from "express";
 import { ExchangeRatesService } from "./exchangeRates.service";
 
 @Controller("exchange-rates")
@@ -6,7 +7,16 @@ export class ExchangeRatesController {
   constructor(private readonly exchangeRatesService: ExchangeRatesService) {}
 
   @Get("available")
-  async getAvailableExchangeRates(): Promise<string[]> {
+  async getAvailableExchangeRates(
+    @Res({ passthrough: true }) response: Response,
+  ): Promise<string[]> {
+    response.setHeader(
+      "Cache-Control",
+      "no-store, no-cache, must-revalidate, max-age=0",
+    );
+    response.setHeader("Pragma", "no-cache");
+    response.setHeader("Expires", "0");
+
     return this.exchangeRatesService.availableExchangeRates();
   }
 }
