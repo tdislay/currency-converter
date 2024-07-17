e2f655027610fe41892647818547db19f66cc6c9
Théo Dislay <mathdis@hotmail.fr>
2024-07-17 11:17:04 +0200

    feat: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,10 @@ async function bootstrap(): Promise<void> {
   await loadConfig();
 
   const app = await NestFactory.create(AppModule);
+
+  // Setup plugins
+  app.enableCors({ origin: "http://localhost:8080" });
+
   await app.listen(3000);
 }
 
